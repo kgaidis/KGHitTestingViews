56c3055849dc000a20b9383ce4ed12ed90a1f5ef
Kris Gaidis <kgaidis@gmail.com>
2015-12-30 12:00:53 -0500

    Added documentation to properties.

--- KGHitTestingViews/KGHitTestingButton.h
+++ KGHitTestingViews/KGHitTestingButton.h
@@ -11,7 +11,34 @@
 
 @interface KGHitTestingButton : UIButton <KGHitTesting>
 
+/**
+ @desc  The minimum width that will be used when hit testing is
+        performed.
+ 
+        Specifying a larger value than the bounds width will
+        increase the hit area of the view.
+ 
+        Specifying a smaller value than the bounds width will
+        cause no effect - the bounds width will be used for hit
+        testing.
+ 
+        The default value causes no effect.
+ */
 @property (nonatomic) IBInspectable CGFloat minimumHitTestWidth;
+
+/**
+ @desc  The minimum height that will be used when hit testing is
+        performed.
+ 
+        Specifying a larger value than the bounds height will
+        increase the hit area of the view.
+ 
+        Specifying a smaller value than the bounds height will
+        cause no effect - the bounds height will be used for hit
+        testing.
+ 
+        The default value causes no effect.
+ */
 @property (nonatomic) IBInspectable CGFloat minimumHitTestHeight;
 
 @end
